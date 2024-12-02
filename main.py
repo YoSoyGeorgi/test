4e1fee6227b3192e6a59399f74382b7026142bd6
Jorge Luis Olvera <78608158+YoSoyGeorgi@users.noreply.github.com>
2024-12-02 11:16:07 -0600

    Update main.py
    New Changes with new endpoints

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from app.dto.pdf_response_dto import PDFResponseDTO
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from app.services.pdf_service import PDFService
+from app.services.pdf_service_imss import PDFServiceIMSS
 from mangum import Mangum
+
+
 
 app = FastAPI()
 handler = Mangum(app)
 
 @app.get("/")
 async def hello():
-    return {"message":"Hola"}
+    return {"message":"Test"}
 
-@app.post("/get_pdf_text", response_model=PDFResponseDTO)
+@app.post("/get-sat", response_model=PDFResponseDTO)
 async def get_pdf_text(request: URLRequest):
     try:
         # Llama al servicio para procesar el archivo
@@ -26,4 +29,20 @@ async def get_pdf_text(request: URLRequest):
         return JSONResponse(content=json_compatible_result)
 
     except Exception as e:
+        raise HTTPException(statusCode=500, detail=f"Error processing PDF: {str(e)}")
+
+@app.post("/get-imss", response_model=PDFResponseDTO)
+async def get_pdf_text(request: URLRequest):
+    try:
+        # Llama al servicio para procesar el archivo
+        result = PDFServiceIMSS.process_pdf(request.url, request.api_key)
+
+        # Serializar el resultado completo, sin excluir `None`
+        json_compatible_result = jsonable_encoder(result.dict(exclude_none=True))
+
+        # Retornar como respuesta JSON
+        return JSONResponse(content=json_compatible_result)
+
+    except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing PDF: {str(e)}")
+
